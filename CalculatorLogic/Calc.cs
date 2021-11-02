b6a2bea97f33c86807c52f863a7764fc4a6c77e8
demisokoya <A00241568@mycambrian.ca>
2021-11-01 22:04:05 -0400

    First successful test for addition
    Calculator was able to add two unique integers

---
+++ CalculatorLogic/Calc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorLogic
+{
+    public class Calc
+    {
+        public static double Add(double left, double right)
+        {
+            return left + right;
+        }
+    }
+}
--- CalculatorUnitTest/CalculatorTests.cs
+++ CalculatorUnitTest/CalculatorTests.cs
@@ -1,13 +1,21 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorLogic;
 
 namespace CalculatorUnitTest
 {
     [TestClass]
     public class CalculatorTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow(1, 2, 3)]
+        [DataRow(2, 3, 5)]
+        [DataRow(3, 4, 7)]
+        [DataRow(4, 5, 9)]
+        public void CalculatorAdditionFUnctionMustAddTwoUniqueSingleDigitNumbers(double left, double right, double expected)
         {
+            double result;
+            result = Calc.Add(left, right);
+            Assert.AreEqual(expected, result);
         }
     }
 }
